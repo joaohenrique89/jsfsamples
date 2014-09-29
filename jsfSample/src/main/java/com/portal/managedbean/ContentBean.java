f01dba3eb0b6b065fde587ab0c573688a3e5c886
Joao Henrique <joaohenrique89@gmail.com>
2014-09-28 22:11:57 -0300

    servlet and jsf samples

--- jsfSample/src/main/java/com/portal/managedbean/ContentBean.java
+++ jsfSample/src/main/java/com/portal/managedbean/ContentBean.java
@@ -3,17 +3,19 @@ package com.portal.managedbean;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@ManagedBean 
-@SessionScoped
-public class Content implements Serializable {
+@ManagedBean
+@RequestScoped
+public class ContentBean implements Serializable {
+
+	private static final long serialVersionUID = 2887839753461911631L;
 
 	@Setter
 	@Getter
 	private String hello = "I am working!";
-	 
+
 }
